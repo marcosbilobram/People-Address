5c3b766650afbacf77f0ad81e94fd85171921423
Marcos Bilobram <marcosbilobram@hotmail.com>
2023-11-06 21:33:32 -0300

    É :O

--- src/main/java/br/com/fiap/epictask/PeopleAddressApplication.java
+++ src/main/java/br/com/fiap/epictask/PeopleAddressApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EpictaskApplication {
+public class PeopleAddressApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EpictaskApplication.class, args);
+		SpringApplication.run(PeopleAddressApplication.class, args);
 	}
 
 }
